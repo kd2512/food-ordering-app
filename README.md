4cd8db5af27daca1a8137dec6847d4353aa4717e
kdivyans <kdivyans@cisco.com>
2023-09-07 17:53:37 +0530

    added tailwing configuration

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
-## A food ordering app in React
+# Food-ifi: A Food Delivery Clone App
 
-Initial Basic Layout
-/\*\*
+Food-ifi is a modern food delivery clone app built with React, React Router DOM, Tailwind CSS, responsive design, and Context API. It allows users to browse a variety of restaurants, view menus, and place food orders seamlessly. This README provides an overview of the app's features, installation instructions, and how to get started.
 
-- Header
-- - Logo
-- - nav links
--
-- Body
-- - Search Component
-- - Restaurant-container
-- - Restaurant cards
--
-- Footer
-- - Copyright
-- - Contact info
-    \*/
+![Food-ifi Preview](demo-preview.png)
+
+## Features
+
+-   **User-friendly Interface:** Food-ifi provides an intuitive and easy-to-navigate interface for users to explore and order food from their favorite restaurants.
+
+-   **Restaurant Listings:** Browse through a list of restaurants available in your area, complete with details like cuisine type, ratings, and delivery time.
+
+-   **Menu Preview:** View restaurant menus with detailed item descriptions, prices, and images.
+
+-   **User Authentication:** Secure user authentication for order placement and tracking. Users can create accounts, log in, and track their order history.
+
+-   **Responsive Design:** Food-ifi is designed to be responsive, ensuring an optimal user experience on both desktop and mobile devices.
+
+-   **Context API:** State management is handled using React's Context API, ensuring efficient data flow throughout the app.
+
+## Installation
+
+To run Food-ifi locally on your machine, follow these steps:
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/yourusername/Food-ifi.git
+    cd Food-ifi
+    ```
+
+2. Install Dependencies:
+
+    ```bash
+    npm install
+     # OR
+    yarn install
+    ```
+
+3. Start the development server:
+
+    ```bash
+    npm start
+     # OR
+    yarn start
+    ```
--- src/views/header-view/index.js
+++ src/views/header-view/index.js
@@ -4,46 +4,46 @@ import { LOGO_URL } from "../../constant";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [authBtn, setAuthBtn] = useState("Login");
+	const [authBtn, setAuthBtn] = useState("Login");
 
-  const handleAuthBtn = () => {
-    if (authBtn === "Login") {
-      setAuthBtn("Logout");
-    } else {
-      setAuthBtn("Login");
-    }
-  };
+	const handleAuthBtn = () => {
+		if (authBtn === "Login") {
+			setAuthBtn("Logout");
+		} else {
+			setAuthBtn("Login");
+		}
+	};
 
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="logo" src={LOGO_URL} alt="app-logo" />
-      </div>
-      <div className="nav-container">
-        <ul className="nav-list">
-          <li className="nav-items">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav-items">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="nav-items">
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li className="nav-items">
-            <Link to="/cart">Cart</Link>
-          </li>
-          <li
-            className="nav-items"
-            style={{ cursor: "pointer" }}
-            onClick={() => handleAuthBtn()}
-          >
-            {authBtn}
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="header flex">
+			<div className="logo-container">
+				<img className="logo" src={LOGO_URL} alt="app-logo" />
+			</div>
+			<div className="nav-container">
+				<ul className="nav-list">
+					<li className="nav-items">
+						<Link to="/">Home</Link>
+					</li>
+					<li className="nav-items">
+						<Link to="/about">About</Link>
+					</li>
+					<li className="nav-items">
+						<Link to="/contact">Contact</Link>
+					</li>
+					<li className="nav-items">
+						<Link to="/cart">Cart</Link>
+					</li>
+					<li
+						className="nav-items"
+						style={{ cursor: "pointer" }}
+						onClick={() => handleAuthBtn()}
+					>
+						{authBtn}
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
